9e326ed79d1934acd3ae8a6346b4e7b87213f552
Alexey Gaskov <agaskov@groupon.com>
2021-08-25 12:10:15 +0100

    User DAO

--- mvc/domain/user_dao.go
+++ mvc/domain/user_dao.go
@@ -1,9 +1,43 @@
 package domain
 
 import (
-	"golang/mvc/domain"
+	"fmt"
+	"log"
+	"net/http"
+
+	"golang/mvc/utils"
 )
 
-func GetUser(userId int64) (domain.User, error) {
-	return domain.GetUser(userId)
+var (
+	users = map[int64]*User{
+		123: {Id: 123, FirstName: "Alexey", LastName: "Gaskov", Email: "alexey.gaskov@gmail.com"},
+		124: {Id: 124, FirstName: "Paul", LastName: "McCartney", Email: "beatles_mc@gmail.com"},
+		125: {Id: 125, FirstName: "Demis", LastName: "Rousses", Email: "the_greek@gmail.com"},
+		126: {Id: 126, FirstName: "Joe", LastName: "Dassen", Email: "joe.gaskov@gmail.com"},
+	}
+
+	UserDao userDaoInterface
+)
+
+func init() {
+	UserDao = &userDao{}
+}
+
+type userDaoInterface interface {
+	GetUser(int64) (*User, *utils.ApplicationError)
+}
+
+type userDao struct{}
+
+func (u *userDao) GetUser(userId int64) (*User, *utils.ApplicationError) {
+	log.Println("we're accessing the database")
+	if user := users[userId]; user != nil {
+		return user, nil
+	}
+
+	return nil, &utils.ApplicationError{
+		Message:    fmt.Sprintf("user %v does not exists", userId),
+		StatusCode: http.StatusNotFound,
+		Code:       "not_found",
+	}
 }
